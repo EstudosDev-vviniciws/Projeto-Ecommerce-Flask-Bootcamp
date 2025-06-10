251268c86590d2c52151c8b921048aaf13c1363c
Vinícius Pinto Gonçalves <vinip8372@gmail.com>
2025-06-09 21:39:11 -0300

    Importa e configura Bcrypt e LoginManager - Bcrypt é instanciado passando o app - LoginManager é iniciado e vinculado ao app - Essas configurações permitem criptografar senhas e gerenciar sessões de login

--- mercado/__init__.py
+++ mercado/__init__.py
@@ -1,10 +1,15 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 db = SQLAlchemy()
+login_manager = LoginManager()
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///mercado.db"
 app.config["SECRET_KEY"] = '14e213928bbca654b936b462'
 db.init_app(app)
+bcrypt = Bcrypt(app)
+login_manager.init_app(app)
 
 from mercado import routes
