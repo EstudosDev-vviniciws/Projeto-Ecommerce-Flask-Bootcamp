a352da3dfeb52cd04c5a63ad9645a4ac320b98f0
Vinícius Pinto Gonçalves <vinip8372@gmail.com>
2025-06-12 22:57:35 -0300

    Adiciona métodos de compra à aplicação - Inclui o método compra_disponivel na classe User para verificar se o saldo do usuário é suficiente para adquirir um produto - Também adiciona o método compra na classe Item, que vincula o item ao usuário atual e subtrai o valor do item do saldo disponível

--- mercado/models.py
+++ mercado/models.py
@@ -32,6 +32,9 @@ class User(db.Model, UserMixin):
     def converte_senha(self, senha_texto_claro):
         return bcrypt.check_password_hash(self.senha, senha_texto_claro)
     
+    def compra_disponivel(self, produto_obj):
+        return self.valor >= produto_obj.preco
+    
 class Item(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nome = db.Column(db.String(length=30), nullable=False, unique=True)
@@ -42,3 +45,8 @@ class Item(db.Model):
     
     def __repr__(self):
         return f"Item {self.nome}"
+    
+    def compra(self, usuario):
+        self.dono = usuario.id
+        usuario.valor -= self.preco
+        db.session.commit()
